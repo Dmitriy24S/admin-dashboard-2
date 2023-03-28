a8bde66128cab5cb6818360ace44fc2d83e8455d
Dmitriy24S <devilins24@gmail.com>
2023-03-28 11:16:25 +0300

    initial setup
    * build setup (add deps)
    * css & tailwind styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>Header</header>
+      <main>Main</main>
+    </div>
+  )
+}
+
+export default App
