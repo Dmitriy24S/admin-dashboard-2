c781230b9bb30c17ac980c1f8d7748b800a23eca
Dmitriy24S <devilins24@gmail.com>
2023-03-29 11:00:16 +0300

    feat: add Orders page + Header component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Inject,
+  Page,
+  PdfExport,
+  Resize,
+  Sort,
+} from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+import { contextMenuItems, ordersData, ordersGrid } from '../data/dummyData'
 
 const Orders = () => {
-  return <div>Orders</div>
+  return (
+    <div className='m-2 md:m-10 p-2 mt-16 md:mt-6 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders' />
+      <GridComponent id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  )
 }
 
 export default Orders
