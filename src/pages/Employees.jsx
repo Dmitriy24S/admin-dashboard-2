6d1be7a2321f47d3806304c497ca9bc3f6704e79
Dmitriy24S <devilins24@gmail.com>
2023-03-30 12:06:20 +0300

    feat: add Employees page/component

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Inject,
+  Page,
+  PdfExport,
+  Resize,
+  Search,
+  Sort,
+  Toolbar,
+} from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+import { contextMenuItems, employeesData, employeesGrid } from '../data/dummyData'
 
 const Employees = () => {
-  return <div>Employees</div>
+  return (
+    <div className='m-10 mt-16 md:mt-10 p-4 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Employees' />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  )
 }
 
 export default Employees
