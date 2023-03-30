1aacd0f67db065370b4a82a31282345a90e7610d
Dmitriy24S <devilins24@gmail.com>
2023-03-30 12:04:17 +0300

    style: change margin/padding for Orders component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -17,7 +17,7 @@ import { contextMenuItems, ordersData, ordersGrid } from '../data/dummyData'
 
 const Orders = () => {
   return (
-    <div className='m-2 md:m-10 p-2 mt-16 md:mt-6 md:p-10 bg-white rounded-3xl'>
+    <div className='m-10 mt-16 md:mt-10 p-4 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Orders' />
       <GridComponent id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
