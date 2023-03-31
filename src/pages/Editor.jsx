1ffd207f45e288fe26480b13caf657f5d36410fb
Dmitriy24S <devilins24@gmail.com>
2023-03-31 23:56:35 +0300

    feat: add Editor page/component
    * syncfusion RichTextEditor Component

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from '@syncfusion/ej2-react-richtexteditor'
+
+import { Header } from '../components'
+import { EditorData } from '../data/dummyData'
 
 const Editor = () => {
-  return <div>Editor</div>
+  return (
+    <div className='m-10 mt-16 md:mt-10 p-10 bg-white rounded-3xl overflow-scroll'>
+      <Header category='App' title='Editor' />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  )
 }
 
 export default Editor
