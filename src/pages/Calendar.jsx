f73af4901d76606c448fc273fd4ef3bca55ba0da
Dmitriy24S <devilins24@gmail.com>
2023-03-31 23:54:07 +0300

    feat: add Calendar page/component
    * syncfusion Schedule Component

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+import {
+  Agenda,
+  Day,
+  DragAndDrop,
+  Inject,
+  Month,
+  Resize,
+  ScheduleComponent,
+  ViewDirective,
+  ViewsDirective,
+  Week,
+  WorkWeek,
+} from '@syncfusion/ej2-react-schedule'
+
+import { Header } from '../components'
+import { scheduleData } from '../data/dummyData'
 
 const Calendar = () => {
-  return <div>Calendar</div>
+  return (
+    <div className='m-10 mt-16 md:mt-10 p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Calendar' />
+      <ScheduleComponent
+        height='650px'
+        eventSettings={{
+          dataSource: scheduleData,
+        }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
+  )
 }
 
 export default Calendar
