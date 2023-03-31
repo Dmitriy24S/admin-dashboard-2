09ec126ca2b123bdfdf0c20037f87bff6c315f61
Dmitriy24S <devilins24@gmail.com>
2023-03-31 23:55:51 +0300

    feat: add Kanban page/component
    * syncfusion Kanban Component

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  KanbanComponent,
+} from '@syncfusion/ej2-react-kanban'
+import { Header } from '../components'
+import { kanbanData, kanbanGrid } from '../data/dummyData'
 
 const Kanban = () => {
-  return <div>Kanban</div>
+  return (
+    <div className='m-10 mt-16 md:mt-10 p-10 bg-white rounded-3xl overflow-scroll'>
+      <Header category='App' title='Kanban' />
+      <KanbanComponent
+        id='kanban'
+        keyField='Status'
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+        width='1000px'
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  )
 }
 
 export default Kanban
