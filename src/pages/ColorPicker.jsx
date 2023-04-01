df3dd11d5bbadcd60b0714869cf50e78b29045d7
Dmitriy24S <devilins24@gmail.com>
2023-04-01 11:05:15 +0300

    feat: add Color Picker page/component
    * syncfusion ColorPicker Component

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,60 @@
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
 import React from 'react'
+import { Header } from '../components'
+
+const change = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex
+  // setSelectedValue(args.currentValue.hex)
+  document.getElementById('value').style.color = args.currentValue.hex
+  document.getElementById('value').textContent = args.currentValue.hex
+}
+
+const CustomColorPicker = ({ id, mode }) => (
+  <ColorPickerComponent
+    id={id}
+    mode={mode}
+    modeSwitcher={false}
+    inline
+    showButtons={false}
+    change={change}
+    // value={'#4c3179'}
+  />
+)
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>
+  return (
+    <div className='m-10 p-10 bg-white rounded-3xl overflow-scroll dark:bg-secondary-dark-bg'>
+      <Header category='App' title='Color Picker' />
+      <div className='text-center'>
+        {}
+        <div
+          id='preview'
+          // !
+        />
+        <p
+          id='value'
+          style={{
+            fontWeight: 'bold',
+            fontSize: '1.2rem',
+            letterSpacing: '1.7px',
+            // color: `#${selectedValue}`,
+          }}
+        >
+          <br />
+        </p>
+        <div className='flex justify-between flex-wrap items-center gap-20'>
+          <div>
+            <p className='dark:text-gray-200'>Inline Pallete</p>
+            <CustomColorPicker id='inline-palette' mode='Palette' />
+          </div>
+          <div>
+            <p className='dark:text-gray-200'>Inline Picker</p>
+            <CustomColorPicker id='inline-picker' mode='Picker' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ColorPicker
