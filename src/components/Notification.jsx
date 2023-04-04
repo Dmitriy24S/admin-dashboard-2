198ca514cc2db46bed8f72cd9acb7c3e068f3d01
Dmitriy24S <devilins24@gmail.com>
2023-04-04 13:58:37 +0300

    feat: add Notification dropdown for Topbar

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+
+import { useStateContext } from '../context/ContextProvider'
+import { chatData } from '../data/dummyData'
+import Button from './Button'
 
 const Notification = () => {
-  return <div>Notification</div>
+  const { currentColor } = useStateContext()
+
+  return (
+    <div className='absolute z-40 right-3.5 top-16 bg-white dark:bg-[#42464D] p-4 rounded-lg w-96 shadow-md'>
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-3 items-center'>
+          <h6 className='font-semibold text-lg dark:text-gray-200'>Notifications</h6>
+          {chatData.length > 0 && (
+            <div
+              className={`font-semibold text-lg`}
+              style={{
+                color: currentColor,
+              }}
+            >
+              {chatData.length} New
+            </div>
+          )}
+        </div>
+        <Button icon={<AiOutlineClose />} size='2xp' borderRadius='50%' />
+      </div>
+      {/* Notifications */}
+      <div className='mt-5'>
+        {chatData?.map((item, index) => (
+          // Notification
+          <a
+            href='#'
+            className='p-3 flex items-center gap-5 leading-8 cursor-pointer hover:bg-light-gray dark:hover:bg-main-dark-bg rounded-lg'
+            key={item.image}
+          >
+            <div className='relative'>
+              <img
+                className='rounded-full h-10 w-10'
+                src={item.image}
+                alt={item.message}
+              />
+              <span
+                // style={{ backgroundColor: item.dotColor }}
+                style={{ backgroundColor: currentColor }}
+                className='absolute inlin-flex rounded-full h-2 w-2 right-0 -top-1'
+              />
+            </div>
+            {/* message content */}
+            <div>
+              <p className='font-semibold dark:text-gray-200'>{item.message}</p>
+              <p className='text-gray-500 dark:text-gray-400 text-sm'>{item.desc}</p>
+              <p className='text-gray-500 dark:text-gray-400 text-sm'>{item.time}</p>
+            </div>
+          </a>
+        ))}
+        <div className='mt-5'>
+          <Button
+            color='white'
+            bgColor={currentColor}
+            text='See all notifications'
+            borderRadius='10px'
+            width='full'
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Notification
