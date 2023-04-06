97a2fc04fde0d1ea27ec8adefbcaa1cec90ce6b5
Dmitriy24S <devilins24@gmail.com>
2023-04-06 22:26:38 +0300

    feat: add Pie Chart page/component
    * syncfusion AccumulationChart Component

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,7 +1,58 @@
+import {
+  AccumulationChartComponent,
+  AccumulationDataLabel,
+  AccumulationLegend,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationTooltip,
+  Inject,
+  PieSeries,
+} from '@syncfusion/ej2-react-charts'
 import React from 'react'
 
-const PieChart = () => {
-  return <div>PieChart</div>
+const PieChart = ({ id, data, legendVisibility, height }) => {
+  return (
+    <AccumulationChartComponent
+      id={id}
+      height={height}
+      tooltip={{ enable: true }}
+      legendSettings={{ visible: legendVisibility, background: 'white' }}
+      background={'transparent'}
+    >
+      <Inject
+        services={[
+          AccumulationLegend,
+          PieSeries,
+          AccumulationDataLabel,
+          AccumulationTooltip,
+        ]}
+      />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name='Sale'
+          dataSource={data}
+          xName='x'
+          yName='y'
+          innerRadius='40%'
+          startAngle={0}
+          endAngle={360}
+          radius='70%'
+          explode
+          explodeOffset='10%'
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#ffff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  )
 }
 
 export default PieChart
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Header, PieChart } from '../../components'
+import { pieChartData } from '../../data/dummyData'
 
 const Pie = () => {
-  return <div>Pie</div>
+  return (
+    <div className='m-4 md:mt-10 mt-16 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl shadow-md max-w-7xl mx-auto'>
+      <Header category='Pie' title='Project Cost Breakdown' />
+      <div className='w-full'>
+        <PieChart id='chart-pie' data={pieChartData} legendVisibility height='full' />
+      </div>
+    </div>
+  )
 }
 
 export default Pie
