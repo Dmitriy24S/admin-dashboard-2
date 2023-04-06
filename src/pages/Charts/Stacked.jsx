797c47aae1bdb3ecd7c82e69ada7d524255fbc05
Dmitriy24S <devilins24@gmail.com>
2023-04-06 22:31:28 +0300

    feat: add Stacked Chart page

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Header, StackedChart } from '../../components'
 
 const Stacked = () => {
-  return <div>Stacked</div>
+  return (
+    <div className='m-4 md:mt-10 mt-16 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl shadow-md max-w-7xl mx-auto'>
+      <Header category='Stacked' title='Revenue Breakdown' />
+      <StackedChart />
+    </div>
+  )
 }
 
 export default Stacked
