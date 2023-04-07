1e94e5e28796e262dba1db0c34918a8778aa46b1
Dmitriy24S <devilins24@gmail.com>
2023-04-07 10:18:39 +0300

    fix: apply theme bg color to body
    * apply theme bg color to body - prevent overscroll bg color clash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import { Footer, Sidebar, ThemeSettings, Topbar } from './components'
 import ThemeSettingsButton from './components/ThemeSettingsButton'
@@ -19,41 +19,49 @@ function App() {
     setIsThemeSettingsOpen((prev) => !prev)
   }, [])
 
+  useEffect(() => {
+    if (currentMode === 'Dark') {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  }, [currentMode])
+
   return (
-    <div className={currentMode === 'Dark' ? 'dark' : ''}>
-      <div className='flex relative dark:bg-main-dark-bg'>
-        {/* Sidebar */}
-        {isSidebarOpen ? (
-          <div className='w-72 fixed z-50 sidebar dark:bg-secondary-dark-bg bg-white '>
-            <Sidebar />
-          </div>
-        ) : (
-          <div className='w-0 dark:bg-secondary-dark-bg'>
-            <Sidebar />
-          </div>
-        )}
-        {/* Main */}
-        <div
-          className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-            isSidebarOpen ? 'md:ml-72 overflow-hidden' : 'flex-2'
-          }`}
-        >
-          <div className='fixed md:static z-40 bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-            <Topbar />
-          </div>
-          {/* Theme Settings */}
-          <div className='dark:bg-main-dark-bg p-4 pt-16 md:pt-10 min-h-screen'>
-            {isThemeSettingsOpen && (
-              <ThemeSettings toggleSettingsMenu={toggleSettingsMenu} />
-            )}
-            {/* Routes */}
-            <RoutesComponent />
-          </div>
-          {/* Theme Settings Button */}
-          <ThemeSettingsButton toggleSettingsMenu={toggleSettingsMenu} />
+    // <div className={currentMode === 'Dark' ? 'dark' : ''}>
+    <div className='flex relative dark:bg-main-dark-bg'>
+      {/* Sidebar */}
+      {isSidebarOpen ? (
+        <div className='w-72 fixed z-50 sidebar dark:bg-secondary-dark-bg bg-white '>
+          <Sidebar />
         </div>
+      ) : (
+        <div className='w-0 dark:bg-secondary-dark-bg'>
+          <Sidebar />
+        </div>
+      )}
+      {/* Main */}
+      <div
+        className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+          isSidebarOpen ? 'md:ml-72 overflow-hidden' : 'flex-2'
+        }`}
+      >
+        <div className='fixed md:static z-40 bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+          <Topbar />
+        </div>
+        {/* Theme Settings */}
+        <div className='dark:bg-main-dark-bg p-4 pt-16 md:pt-10 min-h-screen'>
+          {isThemeSettingsOpen && (
+            <ThemeSettings toggleSettingsMenu={toggleSettingsMenu} />
+          )}
+          {/* Routes */}
+          <RoutesComponent />
+        </div>
+        {/* Theme Settings Button */}
+        <ThemeSettingsButton toggleSettingsMenu={toggleSettingsMenu} />
       </div>
     </div>
+    // </div>
   )
 }
 
